0a9a30a87966089b909955053406ff44310f8113
hankyul2 <consistant1y@ajou.ac.kr>
2021-04-22 09:00:37 +0900

    Feat: add register files to instruction class
    instruction class should have register files to make log for output

--- MySim/src/instruction.py
+++ MySim/src/instruction.py
@@ -1,15 +1,19 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
 class Instruction:
     str_inst: str
 
-    phys_dest: int
-    phys_src1: int
-    phys_src2: int
+    phys_dest: int = field(default=0)
+    phys_src1: int = field(default=0)
+    phys_src2: int = field(default=0)
 
-    old_phys_dest: int
+    old_phys_dest: int = field(default=0)
+
+    seq_no: int = field(default=0)
+
+    registers: int = field(default_factory=lambda: [[0, 0] for reg in range(9)])
 
     @property
     def pc(self):
@@ -17,19 +21,56 @@ class Instruction:
 
     @property
     def op_type(self):
-        return hex(self.str_inst.split(" ")[1])
+        return int(self.str_inst.split(" ")[1])
 
     @property
     def arch_dest(self):
-        return hex(self.str_inst.split(" ")[2])
+        return int(self.str_inst.split(" ")[2])
 
     @property
     def arch_src1(self):
-        return hex(self.str_inst.split(" ")[3])
+        return int(self.str_inst.split(" ")[3])
 
     @property
     def arch_src2(self):
-        return hex(self.str_inst.split(" ")[4])
+        return int(self.str_inst.split(" ")[4])
+
+    @property
+    def FE(self):
+        return self.registers[0]
+
+    @property
+    def DE(self):
+        return self.registers[1]
+
+    @property
+    def RN(self):
+        return self.registers[2]
+
+    @property
+    def DI(self):
+        return self.registers[3]
+
+    @property
+    def IS(self):
+        return self.registers[4]
+
+    @property
+    def RR(self):
+        return self.registers[5]
+
+    @property
+    def EX(self):
+        return self.registers[6]
+
+    @property
+    def WB(self):
+        return self.registers[7]
+
+    @property
+    def CM(self):
+        return self.registers[8]
+
 
 
 
--- MySim/test/test_instruction.py
+++ MySim/test/test_instruction.py
@@ -2,5 +2,14 @@ from MySim.src.instruction import Instruction
 
 
 def test_construction():
-    assert Instruction("2b6420 0 -1 29 14", 0, 0, 0, 0)
+    assert Instruction("2b6420 0 -1 29 14")
+
+def test_register_file():
+    inst = Instruction("2b6420 0 -1 29 14")
+    assert 9 == len(inst.registers)
+    assert 2 == len(inst.FE)
+    assert 0 == inst.FE[0]
+    inst.FE[0] = 3
+    assert 3 == inst.FE[0]
+
 
