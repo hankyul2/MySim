b620cbfbf729d6e3f5662af4ad75e9dd0c1e00ae
hankyul2 <consistant1y@ajou.ac.kr>
2021-04-26 18:31:05 +0900

    Test: Literal Diff Test between sample and valid
    make test function for comparing valid file and output file of gcc and perl.

--- MySim/test/test_cpu.py
+++ MySim/test/test_cpu.py
@@ -17,7 +17,7 @@ def test_run():
     sim = MySim("./cse561sim 60 15 3 ../inputs/sample_input_gcc")
     cpu = CPU(60, 15, 3, sim.codes)
     cpu.run()
-    # assert cpu.clock == 53
+    assert cpu.clock == 59
     assert cpu.seq == 100
 
 
--- MySim/test/test_cse561sim.py
+++ MySim/test/test_cse561sim.py
@@ -20,9 +20,11 @@ def test_execute_program_len():
     sim = MySim("./cse561sim 60 15 3 ../inputs/sample_input_gcc")
     assert 110 == sim.execute_program('../outputs/sample_output_gcc')[0]
 
+
 def test_execute_program_len2():
-    sim = MySim("./cse561sim 1 1 1 ../inputs/val_trace_gcc1")
+    sim = MySim("./cse561sim 2 2 1 ../inputs/val_trace_gcc1")
     assert 10010 == sim.execute_program('../outputs/val_output_gcc1')[0]
+
 
 def test_execute_program_len3():
     sim = MySim("./cse561sim 256 64 40 ../inputs/val_trace_perl1")
@@ -42,9 +44,8 @@ def test_execute_program_diff():
         assert out == val
 
 
-@pytest.mark.skip(reason="There is bugs in sample output files")
 def test_execute_program_diff2():
-    sim = MySim("./cse561sim 128 16 5 ../inputs/sample_input_perl")
+    sim = MySim("./cse561sim 60 15 3 ../inputs/sample_input_perl")
     assert 110 == sim.execute_program('../outputs/sample_output_perl')[0]
     with open('../valid/sample_output_perl', 'rt') as f:
         valid_output = f.readlines()
